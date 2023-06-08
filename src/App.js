653031225be85ec5823c6c6a5cb3633215ed303e
Jordyn Fulbright <jordynfulbright@gmail.com>
2023-06-07 20:48:16 -0400

    gh pages edits

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import About from './pages/About';
+// import GetInvolved from './pages/GetInvolved';
+// import Team from './pages/Team';
+// import Header from './components/Header';
+// import Home from './pages/Home';
+// import Footer from './components/Footer';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div className="App">
+//    <BrowserRouter>
+//         <div >
+//         <Routes>
+         
+//            <Route path="/" element={<Home/>} />
+//            <Route path="/about" element={<About/>} />
+//            <Route path="/team" element={<Team/>} />
+//            <Route path="/get-involved" element={<GetInvolved/>} />
+            
+//         </Routes>
+//         </div>
+//         <Footer />
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import logo from './logo.svg';
 import './App.css';
 import About from './pages/About';
@@ -6,24 +38,22 @@ import Team from './pages/Team';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-   <BrowserRouter>
+      <Router basename="/home">
         <div >
-        <Routes>
-         
-           <Route path="/" element={<Home/>} />
-           <Route path="/about" element={<About/>} />
-           <Route path="/team" element={<Team/>} />
-           <Route path="/get-involved" element={<GetInvolved/>} />
-            
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/team" element={<Team/>} />
+            <Route path="/get-involved" element={<GetInvolved/>} />
+          </Routes>
         </div>
         <Footer />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
--- src/components/NavPane.js
+++ src/components/NavPane.js
@@ -13,7 +13,7 @@ return (
 <div className="navPane">
 <div className = "navItemPane"><Link to = '/about' className = "linkNavPane">About</Link></div>
 <div className = "navItemPane"><Link to = '/team' className = "linkNavPane">Team</Link></div>
-<div className = "navItemPane"><Link to = '/get-involved' className = "linkNavPanes">Get Involved</Link></div>
+<div className = "navItemPane"><Link to = '/get-involved' className = "linkNavPane">Get Involved</Link></div>
 
 
 </div>
